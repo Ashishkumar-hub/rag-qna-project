8933286275226ab14c38920c307d6661b13f5a38
Ashishkumar-hub <singh.ashish.kr96@gmail.com>
2025-02-23 02:50:08 +0530

    chore: improve dotenv loading for better environment management

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
-load_dotenv()
+# Load environment variables from the correct path
+dotenv_path = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "configs", ".env"
+)
+load_dotenv(dotenv_path)
 
-# Retrieve the database URL from environment variables,
+# Retrieve the database URL from environment variables
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
